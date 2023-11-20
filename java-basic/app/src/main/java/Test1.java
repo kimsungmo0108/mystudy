0be5c0d1bf338d14935af41cb08211fc295fa87a
kimsungmo0108 <rlatjdah8585@daum.net>
2023-11-20 18:00:00 +0900

    .

---
+++ java-basic/app/src/main/java/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+  public static void main(String[] args){
+    System.out.println(100);
+    System.out.println(3.14);
+    System.out.println('가');
+    System.out.println("안녕하세요 !!!!");
+    System.out.println(true);
+    System.out.println(false);
+  }
+}
