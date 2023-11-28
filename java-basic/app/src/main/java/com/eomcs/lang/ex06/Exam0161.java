128a4e4740ff448ba584811e3b48800ddb14de1c
kimsungmo0108 <rlatjdah8585@daum.net>
2023-11-28 14:01:19 +0900

    11-28

--- java-basic/app/src/main/java/com/eomcs/lang/ex06/Exam0161.java
+++ java-basic/app/src/main/java/com/eomcs/lang/ex06/Exam0161.java
@@ -2,7 +2,7 @@ package com.eomcs.lang.ex06;
 
 import java.util.Scanner;
 
-//# 흐름 제어문 - if ~ else if ~ else if ~ else
+// # 흐름 제어문 - if ~ else if ~ else if ~ else
 //
 public class Exam0161 {
   public static void main(String[] args) {
@@ -14,18 +14,15 @@ public class Exam0161 {
     // if ~ else ~ 만 있다면 블록으로 묶지 않아도 된다.
     if (age < 8)
       System.out.println("아동입니다.");
-    else 
-      if (age < 14)
-        System.out.println("어린이입니다.");
-      else 
-        if (age < 19)
-          System.out.println("청소년입니다.");
-        else 
-          if (age < 65)
-            System.out.println("성인입니다.");
-          else
-            System.out.println("노인입니다.");
-    
+    else if (age < 14)
+      System.out.println("어린이입니다.");
+    else if (age < 19)
+      System.out.println("청소년입니다.");
+    else if (age < 65)
+      System.out.println("성인입니다.");
+    else
+      System.out.println("노인입니다.");
+
     keyScan.close();
   }
 }
