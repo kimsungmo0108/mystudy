5100cc4b9802b9d925c595ecfabd2df02b6e2d1d
kimsungmo0108 <rlatjdah8585@daum.net>
2023-11-29 18:06:59 +0900

    app-07

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class App {
 
+  // 애플리케이션 객체(클래스) App을 실행할 때 다음 변수를 미리 준비해 둔다.
+  static final String ANSI_CLEAR = "\033[0m";
+  static final String ANSI_BOLD_RED = "\033[1;31m";
+  static final String ANSI_RED = "\033[0;31m";
+  static final String APP_TITLE = ANSI_BOLD_RED + "[과제관리 시스템]" + ANSI_CLEAR + "\n";
+  static final String MENU4 = ANSI_RED + "4. 종료" + ANSI_CLEAR;
+
+  static final String[] MENU_ARR = new String[]{
+      APP_TITLE,
+      "1. 과제",
+      "2. 게시글",
+      "3. 도움말",
+      MENU4
+  };
+  static Scanner numScan = new Scanner(System.in);
+
   public static void main(String[] args) {
 
-    String ANSI_CLEAR = "\033[0m";
-    String ANSI_BOLD_RED = "\033[1;31m";
-    String ANSI_RED = "\033[0;31m";
-    String appTitle = "[과제관리 시스템]";
-
-    String[] menuArr = new String[]{
-        ANSI_BOLD_RED + appTitle + ANSI_CLEAR + "\n",
-        "1. 과제",
-        "2. 게시글",
-        "3. 도움말",
-        ANSI_RED + "4. 종료" + ANSI_CLEAR
-    };
-
-    for (String item : menuArr) {
-      System.out.println(item);
-    }
-
-    Scanner numScan = new Scanner(System.in);
+    // 코드를 기능 단위로 묶어 메소드로 정의하면
+    // 메소드의 이름을 통해 해당 기능을 쉽게 유추할 수 있어 유지보수에 좋다.
+    printMenu();
     menuloop:
     while (true) {
-      System.out.print("> ");
-      String num = numScan.next();
+      // static 대신에 printMenu(MENU_ARR); 해도된다.
+      String num = prompt(numScan);
+      
       switch (num) {
         case "1":
           System.out.println("과제입니다.");
@@ -42,14 +44,30 @@ public class App {
           System.out.println("종료합니다.");
           break menuloop;
         case "menu":
-          for (String item : menuArr) {
-            System.out.println(item);
-          }
+          printMenu();
           break;
         default:
           System.out.println("메뉴 번호가 옳지 않습니다.");
       }
     }
     numScan.close();
+  }
+
+  static void printMenu() {
+
+    // 메뉴를 저장한 변수를 메소드 안에 두는 대신에
+    // 클래스 블록 안에 두면
+    // printMenu()를 호출할 때마다 변수를 만들지 않기 때문에 실행 속도나 메모리 부분에서
+    // 훨씬 효율적이다.
+    // 보통 메소드 호출될 때마다 값이 바뀌는 변수가 아니라 고정 값을 갖는 변수인 경우
+    // 메소드 밖에 도는 것이 좋다.
+    for (String item : MENU_ARR) {
+      System.out.println(item);
+    }
+  }
+
+  static String prompt(Scanner numScan) {
+    System.out.print("> ");
+    return numScan.next();
   }
 }
