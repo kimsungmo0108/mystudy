09e07ce063d8afb5523bfbd7f8032a2d668dd288
kimsungmo0108 <rlatjdah8585@daum.net>
2023-12-13 18:16:13 +0900

    baekjoon.level04.exam09

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam09/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam09/Main.java
@@ -5,10 +5,27 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
-
-    System.out.println(c);
-
+    int n = scanner.nextInt();
+    int m = scanner.nextInt();
+    int[] num = new int[n];
+    int a, b;
+    int temp;
+    for (int i = 0; i < n; i++) {
+      num[i] = i;
+      // System.out.println(num[i] + 1);
+    }
+    for (int i = 0; i < m; i++) {
+      a = scanner.nextInt() - 1;
+      b = scanner.nextInt() - 1;
+      for (; a < b; a++, b--) {
+        temp = num[a];
+        num[a] = num[b];
+        num[b] = temp;
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      System.out.print((num[i] + 1) + " ");
+    }
     scanner.close();
   }
 }
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam10/Main.java
@@ -0,0 +1,25 @@
+package algorithm.test.baekjoon.level04.exam10;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    double[] score = new double[n];
+    double max = 0.0;
+    double total = 0.0;
+    for (int i = 0; i < n; i++) {
+      score[i] = scanner.nextDouble();
+    }
+    for (int i = 0; i < n - 1; i++) {
+      max = score[i] > score[i + 1] ? score[i] : score[i + 1];
+    }
+    for (int i = 0; i < n; i++) {
+      score[i] = score[i] / max * 100;
+      total += score[i];
+    }
+    System.out.println(total / n);
+    scanner.close();
+  }
+}
