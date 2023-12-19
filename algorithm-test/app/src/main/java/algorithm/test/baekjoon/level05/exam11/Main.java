b78763244afc7984fe2bfaf5c4c2c05c886ac227
kimsungmo0108 <rlatjdah8585@daum.net>
2023-12-19 17:43:37 +0900

    baekjoon.level05.exam11

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/exam11/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/exam11/Main.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    String str;
+
+    while (sc.hasNext()) {
+      str = sc.nextLine();
+      System.out.println(str);
+      if (str.equalsIgnoreCase("exit")) {
+        break;
+      }
+    }
 
     sc.close();
   }
