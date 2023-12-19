810a0889b809776636918fc87c3e33938c9a18a5
kimsungmo0108 <rlatjdah8585@daum.net>
2023-12-19 18:54:33 +0900

    baekjoon.level05.exam11

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/exam11/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/exam11/Main.java
@@ -10,7 +10,7 @@ public class Main {
     while (sc.hasNext()) {
       str = sc.nextLine();
       System.out.println(str);
-      if (str.equalsIgnoreCase("exit")) {
+      if (str.equals("exit")) {
         break;
       }
     }
