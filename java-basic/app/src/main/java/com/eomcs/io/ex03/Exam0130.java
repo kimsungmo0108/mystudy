c361aed8fbcd0ace94b7a01532cd20f1c6b01030
kimsungmo0108 <rlatjdah8585@daum.net>
2024-01-05 14:15:14 +0900

    app-35

---
+++ java-basic/app/src/main/java/com/eomcs/io/ex03/Exam0130.java
@@ -0,0 +1,24 @@
+// character stream - 문자 단위로 읽기
+package com.eomcs.io.ex03;
+
+import java.io.FileReader;
+
+public class Exam0130 {
+
+  public static void main(String[] args) throws Exception {
+
+    FileReader in = new FileReader("sample/utf8.txt");
+
+
+    int ch;
+    while ((ch = in.read()) != -1) {
+      System.out.printf("%04x ", ch);
+    }
+
+
+    in.close();
+
+
+  }
+}
+
