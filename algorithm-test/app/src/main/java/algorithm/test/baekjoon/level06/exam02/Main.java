21a5bc6768eabed78c458740cecae42dae024506
kimsungmo0108 <rlatjdah8585@daum.net>
2024-01-11 18:42:08 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam02/Main.java
@@ -2,11 +2,8 @@ package algorithm.test.baekjoon.level06.exam02;
 
 public class Main {
   public static void main(String[] args) {
-    Thread thread = new Thread(() -> {
-      System.out.println("작업 스레드가 실행됩니다");
-      System.out.println("작업 스레드가 실행됩니다");
-      System.out.println("작업 스레드가 실행됩니다");
-    });
-    thread.start();
+
   }
 }
+
+
