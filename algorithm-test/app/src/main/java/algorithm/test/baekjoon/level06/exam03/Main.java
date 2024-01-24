b9198b6269e10ce6d1d7d346bf04b7498fd5bf06
kimsungmo0108 <rlatjdah8585@daum.net>
2024-01-24 17:24:29 +0900

    baekjoon.level06.exam03

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam03/Main.java
@@ -9,11 +9,22 @@ public class Main {
       for (int j = n - i; j > 0; j--) {
         System.out.print(" ");
       }
-      for (int k = 0; k < (i - 1) * 2 + 1; k = k + 2) {
+      for (int k = 0; k < ((i - 1) * 2) + 1; k++) {
         System.out.print("*");
       }
       System.out.print("\n");
     }
+
+    for (int i = n - 1; i > 0; i--) {
+      for (int j = 1; j < n - i + 1; j++) {
+        System.out.print(" ");
+      }
+      for (int k = ((i - 1) * 2) + 1; k > 0; k--) {
+        System.out.print("*");
+      }
+      System.out.print("\n");
+    }
+    sc.close();
   }
 }
 
