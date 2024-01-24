71c26da8072f2ed8061e27aee6832e24beb95b86
kimsungmo0108 <rlatjdah8585@daum.net>
2024-01-24 12:30:09 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam03/Main.java
@@ -1,9 +1,22 @@
 package algorithm.test.baekjoon.level06.exam03;
 
-import java.io.IOException;
 
 public class Main {
-  public static void main(String[] args) throws IOException {}
+  public static void main(String[] args) {
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+    int n = sc.nextInt();
+    for (int i = 0; i <= n; i = i + 2) {
+      for (int k = n - 1; k > 0; k--) {
+        System.out.print(" ");
+      }
+
+      System.out.print("*");
+      for (int j = 1; j < i + 1; j++) {
+        System.out.print("*");
+      }
+      System.out.print("\n");
+    }
+  }
 }
 
 
