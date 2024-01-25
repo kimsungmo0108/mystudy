870d957a70b667de87b0abac19f0d0e2d14e9d3d
kimsungmo0108 <rlatjdah8585@daum.net>
2024-01-25 18:24:17 +0900

    baekjoon.level06.exam04

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam04/Main.java
@@ -1,9 +1,23 @@
 package algorithm.test.baekjoon.level06.exam04;
 
-import java.io.IOException;
-
 public class Main {
-  public static void main(String[] args) throws IOException {}
+  public static void main(String[] args) {
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+    String str = sc.nextLine();
+    char[] chars = new char[str.length()];
+    byte b = 0;
+    str.getChars(0, str.length(), chars, 0);
+    for (int i = 0; i <= (str.length() / 2); i++) {
+      if (chars[i] == chars[str.length() - 1 - i]) {
+        b = 1;
+      } else if (chars[i] != chars[str.length() - 1 - i]) {
+        b = 0;
+        break;
+      }
+    }
+
+    System.out.println(b);
+  }
 }
 
 
