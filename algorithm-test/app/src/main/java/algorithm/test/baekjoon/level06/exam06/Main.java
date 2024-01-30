3d153b3776d69571b045a81e4a5658df5b109bec
kimsungmo0108 <rlatjdah8585@daum.net>
2024-01-30 16:43:52 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam06/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam06/Main.java
@@ -2,7 +2,40 @@ package algorithm.test.baekjoon.level06.exam06;
 
 public class Main {
   public static void main(String[] args) {
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+    String str = sc.nextLine();
+    int count = 0;
 
+    for (int i = 0; i < str.length();) {
+      if (str.charAt(i) == 'c') {
+        if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
+          i += 2;
+        } else {
+          i++;
+        }
+      } else if (str.charAt(i) == 'd') {
+        if (str.charAt(i + 1) == 'z' && str.charAt(i + 2) == '=') {
+          i += 3;
+        } else if (str.charAt(i + 1) == '-') {
+          i += 2;
+        } else {
+          i++;
+        }
+      } else if (str.charAt(i) == 'l' && str.charAt(i + 1) == 'j') {
+        i += 2;
+      } else if (str.charAt(i) == 'n' && str.charAt(i + 1) == 'j') {
+        i += 2;
+      } else if (str.charAt(i) == 's' && str.charAt(i + 1) == '=') {
+        i += 2;
+      } else if (str.charAt(i) == 'z' && str.charAt(i + 1) == '=') {
+        i += 2;
+      } else {
+        i++;
+      }
+      count++;
+    }
+    System.out.println(count);
+    sc.close();
   }
 }
 
