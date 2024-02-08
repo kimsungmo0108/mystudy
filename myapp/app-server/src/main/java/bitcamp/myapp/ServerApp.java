42cea9d9675a8d2ff4512d46f9561789534b6fd8
kimsungmo0108 <rlatjdah8585@daum.net>
2024-02-08 11:48:22 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -1,52 +1,67 @@
 package bitcamp.myapp;
 
-import bitcamp.RequestException;
-import bitcamp.myapp.dao.json.AssignmentDaoImpl;
-import bitcamp.myapp.dao.json.BoardDaoImpl;
-import bitcamp.myapp.dao.json.MemberDaoImpl;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import bitcamp.menu.MenuGroup;
+import bitcamp.myapp.dao.AssignmentDao;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.dao.mysql.AssignmentDaoImpl;
+import bitcamp.myapp.dao.mysql.BoardDaoImpl;
+import bitcamp.myapp.dao.mysql.MemberDaoImpl;
+import bitcamp.myapp.handler.HelpHandler;
+import bitcamp.myapp.handler.assignment.AssignmentAddHandler;
+import bitcamp.myapp.handler.assignment.AssignmentDeleteHandler;
+import bitcamp.myapp.handler.assignment.AssignmentListHandler;
+import bitcamp.myapp.handler.assignment.AssignmentModifyHandler;
+import bitcamp.myapp.handler.assignment.AssignmentViewHandler;
+import bitcamp.myapp.handler.board.BoardAddHandler;
+import bitcamp.myapp.handler.board.BoardDeleteHandler;
+import bitcamp.myapp.handler.board.BoardListHandler;
+import bitcamp.myapp.handler.board.BoardModifyHandler;
+import bitcamp.myapp.handler.board.BoardViewHandler;
+import bitcamp.myapp.handler.member.MemberAddHandler;
+import bitcamp.myapp.handler.member.MemberDeleteHandler;
+import bitcamp.myapp.handler.member.MemberListHandler;
+import bitcamp.myapp.handler.member.MemberModifyHandler;
+import bitcamp.myapp.handler.member.MemberViewHandler;
+import bitcamp.util.Prompt;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class ServerApp {
 
-  ExecutorService threadPool = Executors.newCachedThreadPool();
-  HashMap<String, Object> daoMap = new HashMap<>();
-  Gson gson;
+  Prompt prompt = new Prompt(System.in);
 
-  public ServerApp() {
-    daoMap.put("board", new BoardDaoImpl("board.json"));
-    daoMap.put("greeting", new BoardDaoImpl("greeting.json"));
-    daoMap.put("assignment", new AssignmentDaoImpl("assignment.json"));
-    daoMap.put("member", new MemberDaoImpl("member.json"));
+  BoardDao boardDao;
+  BoardDao greetingDao;
+  AssignmentDao assignmentDao;
+  MemberDao memberDao;
 
-    gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+  MenuGroup mainMenu;
+
+  ServerApp() {
+    prepareDatabase();
+    prepareMenu();
   }
 
   public static void main(String[] args) {
+    System.out.println("과제관리 시스템 서버 실행!");
     new ServerApp().run();
   }
 
-  void run() {
-    System.out.println("[과제관리 서버시스템]");
+  void prepareDatabase() {
+    try {
+      Connection con = DriverManager.getConnection(
+          //"jdbc:mysql://localhost/studydb", "study", "Bitcamp!@#123");
+          "jdbc:mysql://db-ld27b-kr.vpc-pub-cdb.ntruss.com/studydb", "study", "Bitcamp!@#123");
 
-    try (ServerSocket serverSocket = new ServerSocket(8889)) {
-
-      System.out.println("서버 실행!");
-
-      while (true) {
-        Socket socket = serverSocket.accept();
-        threadPool.execute(() -> service(socket));
-      }
+      boardDao = new BoardDaoImpl(con, 1);
+      greetingDao = new BoardDaoImpl(con, 2);
+      assignmentDao = new AssignmentDaoImpl(con);
+      memberDao = new MemberDaoImpl(con);
 
     } catch (Exception e) {
       System.out.println("통신 오류!");
@@ -54,85 +69,82 @@ public class ServerApp {
     }
   }
 
-  void service(Socket socket) {
+  void prepareMenu() {
+    mainMenu = MenuGroup.getInstance("메인");
 
+    MenuGroup assignmentMenu = mainMenu.addGroup("과제");
+    assignmentMenu.addItem("등록", new AssignmentAddHandler(assignmentDao, prompt));
+    assignmentMenu.addItem("조회", new AssignmentViewHandler(assignmentDao, prompt));
+    assignmentMenu.addItem("변경", new AssignmentModifyHandler(assignmentDao, prompt));
+    assignmentMenu.addItem("삭제", new AssignmentDeleteHandler(assignmentDao, prompt));
+    assignmentMenu.addItem("목록", new AssignmentListHandler(assignmentDao, prompt));
+
+    MenuGroup boardMenu = mainMenu.addGroup("게시글");
+    boardMenu.addItem("등록", new BoardAddHandler(boardDao, prompt));
+    boardMenu.addItem("조회", new BoardViewHandler(boardDao, prompt));
+    boardMenu.addItem("변경", new BoardModifyHandler(boardDao, prompt));
+    boardMenu.addItem("삭제", new BoardDeleteHandler(boardDao, prompt));
+    boardMenu.addItem("목록", new BoardListHandler(boardDao, prompt));
+
+    MenuGroup memberMenu = mainMenu.addGroup("회원");
+    memberMenu.addItem("등록", new MemberAddHandler(memberDao, prompt));
+    memberMenu.addItem("조회", new MemberViewHandler(memberDao, prompt));
+    memberMenu.addItem("변경", new MemberModifyHandler(memberDao, prompt));
+    memberMenu.addItem("삭제", new MemberDeleteHandler(memberDao, prompt));
+    memberMenu.addItem("목록", new MemberListHandler(memberDao, prompt));
+
+    MenuGroup greetingMenu = mainMenu.addGroup("가입인사");
+    greetingMenu.addItem("등록", new BoardAddHandler(greetingDao, prompt));
+    greetingMenu.addItem("조회", new BoardViewHandler(greetingDao, prompt));
+    greetingMenu.addItem("변경", new BoardModifyHandler(greetingDao, prompt));
+    greetingMenu.addItem("삭제", new BoardDeleteHandler(greetingDao, prompt));
+    greetingMenu.addItem("목록", new BoardListHandler(greetingDao, prompt));
+
+    mainMenu.addItem("도움말", new HelpHandler(prompt));
+  }
+
+  void run() {
+    try (ServerSocket serverSocket = new ServerSocket(8888)) {
+
+      while (true) {
+        Socket socket = serverSocket.accept();
+        processRequest(socket);
+      }
+
+    } catch (Exception e) {
+      System.out.println("서버 소켓 생성 오류!");
+      e.printStackTrace();
+    }
+  }
+
+  void processRequest(Socket socket) {
     try (Socket s = socket;
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        DataInputStream in = new DataInputStream(s.getInputStream())) {
 
-      System.out.printf("[%s] 클라이언트와 연결됨!\n", Thread.currentThread().getName());
-
-      processRequest(in, out);
-
-      System.out.printf("[%s] 클라이언트 연결 종료!\n", Thread.currentThread().getName());
+      out.writeUTF("[과제관리 시스템]");
+      String request = in.readUTF();
+      if (request.equals("quit")) {
+        out.writeUTF("[[quit!]]");
+      }
+      System.out.println(request);
 
     } catch (Exception e) {
-      System.out.printf("[%s] 클라이언트 연결 오류!\n", Thread.currentThread().getName());
+      System.out.println("클라이언 통신 오류!");
+      e.printStackTrace();
     }
   }
 
-  void processRequest(DataInputStream in, DataOutputStream out) throws IOException {
+//  void run() {
+//    while (true) {
+//      try {
+//        mainMenu.execute(prompt);
+//        prompt.close();
+//        break;
+//      } catch (Exception e) {
+//        System.out.println("예외 발생!");
+//      }
+//    }
+//  }
 
-    System.out.println("[클라이언트 요청]");
-    String dataName = in.readUTF();
-    String command = in.readUTF();
-    String value = in.readUTF();
-
-    try {
-      Object dao = daoMap.get(dataName);
-      if (dao == null) {
-        throw new RequestException("요청 데이터가 없습니다!");
-      }
-      System.out.printf("데이터: %s\n", dataName);
-
-      Method commandHandler = findMethod(dao.getClass(), command);
-      System.out.printf("메서드: %s\n", commandHandler.getName());
-
-      Object[] args = getArguments(commandHandler, value);
-
-//      Class<?> returnType = commandHandler.getReturnType();
-//      System.out.printf("리턴: %s\n", returnType.getName());
-
-      // 메서드를 호출한다.
-      Object returnValue = commandHandler.invoke(dao, args);
-
-      out.writeUTF("200");
-      out.writeUTF(gson.toJson(returnValue));
-      System.out.println("클라이언트에게 응답 완료!");
-
-    } catch (RequestException e) {
-      out.writeUTF("400");
-      out.writeUTF(gson.toJson(e.getMessage()));
-
-    } catch (Exception e) {
-      out.writeUTF("500");
-      out.writeUTF(gson.toJson(e.getMessage()));
-    }
-  }
-
-  Method findMethod(Class<?> clazz, String name) {
-    Method[] methods = clazz.getDeclaredMethods();
-    for (Method m : methods) {
-      if (m.getName().equals(name)) {
-        return m;
-      }
-    }
-    throw new RequestException("요청 메서드가 없습니다!");
-  }
-
-  Object[] getArguments(Method m, String json) {
-    Parameter[] params = m.getParameters();
-    System.out.printf("파라미터 개수: %d\n", params.length);
-
-    Object[] args = new Object[params.length];
-
-    if (params.length > 0) {
-      Class<?> paramType = params[0].getType();
-      Object paramValue = gson.fromJson(json, paramType);
-      args[0] = paramValue;
-    }
-
-    return args;
-  }
 }
-
