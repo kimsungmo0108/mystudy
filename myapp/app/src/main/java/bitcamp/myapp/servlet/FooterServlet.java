b9094c21d4ff9e34538d43c3309c0a520fb79096
kimsungmo0108 <rlatjdah8585@daum.net>
2024-02-23 17:13:06 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/servlet/FooterServlet.java
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/FooterServlet.java
@@ -1,2 +1,24 @@
-package bitcamp.myapp.servlet;public class FooterServlet {
+package bitcamp.myapp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/footer")
+public class FooterServlet extends HttpServlet {
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("<footer>");
+    out.println("  <address> 비트캠프:서울 강남구 강남대로94길 20, 삼오빌딩 5층 </address>");
+    out.println("  <p> & copy; 2024 비트캠프 네이버 클라우드 데브옵스 5기 </p>");
+    out.println("</footer>");
+  }
 }
