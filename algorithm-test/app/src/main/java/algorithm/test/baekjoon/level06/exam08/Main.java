1b6efa543bf84e5b85a1a5b9f9ec78e30d85d6c4
kimsungmo0108 <rlatjdah8585@daum.net>
2024-02-27 17:13:41 +0900

    .

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam08/Main.java
@@ -0,0 +1,12 @@
+package algorithm.test.baekjoon.level06.exam08;
+
+public class Main {
+  public static void main(String[] args) {
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+
+
+    sc.close();
+  }
+}
+
+
