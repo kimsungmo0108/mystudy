7b53282bbfa714c445fcd8256b572e76a7a78845
kimsungmo0108 <rlatjdah8585@daum.net>
2024-03-04 18:24:34 +0900

    >

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam01/Main.java
@@ -3,6 +3,8 @@ package algorithm.test.baekjoon.level07.exam01;
 public class Main {
   public static void main(String[] args) {
     java.util.Scanner sc = new java.util.Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
 
 
     sc.close();
