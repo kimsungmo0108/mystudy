a80a7c61817a8f5cfda0a278b0864546eef82d64
kimsungmo0108 <rlatjdah8585@daum.net>
2024-03-14 16:31:24 +0900

    .

--- mvc-app/app/src/main/java/bitcamp/config/AppWebApplicationInitializer.java
+++ mvc-app/app/src/main/java/bitcamp/config/AppWebApplicationInitializer.java
@@ -5,64 +5,34 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.AbstractContextLoaderInitializer;
-import org.springframework.web.context.ContextLoader;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
-public class WebApplicationInitializerImpl extends AbstractDispatcherServletInitializer {
+public class AppWebApplicationInitializer extends
+    AbstractAnnotationConfigDispatcherServletInitializer {
 
-  private static Log log = LogFactory.getLog(WebApplicationInitializerImpl.class);
-  AnnotationConfigWebApplicationContext rootContext;
-
-  @Override
-  protected WebApplicationContext createRootApplicationContext() {
-    rootContext = new AnnotationConfigWebApplicationContext();
-    rootContext.register(RootConfig.class);
-    rootContext.refresh();
-    return rootContext;
-  }
+  private static Log log = LogFactory.getLog(AppWebApplicationInitializer.class);
 
 
   @Override
-  protected WebApplicationContext createServletApplicationContext() {
-    AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-    appContext.setParent(rootContext);
-    appContext.register(AppConfig.class);
-    appContext.refresh();
-    return  appContext;
+  protected Class<?>[] getRootConfigClasses() {
+    return null;
   }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[]{AppConfig.class};
+  }
+
   @Override
   protected String[] getServletMappings() {
     return new String[]{"/app/*"};
   }
-
   @Override
   protected String getServletName() {
     return "app";
   }
-
-  @Override
-  public void onStartup(ServletContext servletContext) throws ServletException {
-
-    log.debug("onStartup() 호출됨!");
-    
-    // 수퍼 클래스의 구현 내용은 그대로 유지해야한다
-    // 왜? 수퍼 클래스의 메소드에서 ContextLoaderListener 객체를 만들기 때문이다
-    super.onStartup(servletContext);
-
-    AnnotationConfigWebApplicationContext adminContext = new AnnotationConfigWebApplicationContext();
-    adminContext.setParent(rootContext);
-    adminContext.register(AdminConfig.class);
-    adminContext.refresh();
-    Dynamic adminServletRegistration = servletContext.addServlet("admin",
-        new DispatcherServlet(adminContext));
-    adminServletRegistration.addMapping("/admin/*");
-    adminServletRegistration.setLoadOnStartup(1);
-  }
-
 }
