4bd0bb9ca0b772eaa52f7de66948c07f258b1c39
kimsungmo0108 <rlatjdah8585@daum.net>
2024-03-21 14:52:46 +0900

    app-76

--- myapp/app/src/main/java/bitcamp/myapp/controller/GlobalControllerAdvice.java
+++ myapp/app/src/main/java/bitcamp/myapp/controller/GlobalControllerAdvice.java
@@ -1,10 +1,14 @@
 package bitcamp.myapp.controller;
 
 import java.beans.PropertyEditorSupport;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.Date;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
@@ -20,4 +24,17 @@ public class GlobalControllerAdvice {
         });
   }
 
+  @ExceptionHandler
+  public ModelAndView exceptionrHandler(Exception e) {
+    ModelAndView mv = new ModelAndView();
+    mv.addObject("message", e.getMessage());
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter out = new PrintWriter(stringWriter);
+    e.printStackTrace(out);
+    mv.addObject("detail", stringWriter.toString());
+
+    mv.setViewName("/error.jsp");
+    return mv;
+  }
 }
