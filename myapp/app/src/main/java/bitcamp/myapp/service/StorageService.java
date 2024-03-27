1d4387b7e3c3c99f130ed35f17b24f66354cf188
kimsungmo0108 <rlatjdah8585@daum.net>
2024-03-27 15:06:59 +0900

    .

---
+++ myapp/app/src/main/java/bitcamp/myapp/service/StorageService.java
@@ -0,0 +1,9 @@
+package bitcamp.myapp.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+
+  String upload(MultipartFile multipartFile) throws Exception;
+  
+}
---
+++ myapp/app/src/main/java/bitcamp/myapp/service/impl/NcpStorageService.java
@@ -0,0 +1,45 @@
+package bitcamp.myapp.service.impl;
+
+import bitcamp.myapp.service.StorageService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class NcpStorageService implements StorageService, InitializingBean {
+
+  private static Log log = LogFactory.getLog(NcpStorageService.class);
+  String endPoint;
+  String regionName;
+
+  String accessKey;
+
+  String secretKey;
+
+  public NcpStorageService(
+      @Value("${ncp.ss.endpoint}") String endPoint,
+      @Value("${ncp.ss.regionname}") String regionName,
+      @Value("${ncp.accesskey}") String accessKey,
+      @Value("${ncp.secretkey}") String secretKey) {
+    this.endPoint = endPoint;
+    this.regionName = regionName;
+    this.accessKey = accessKey;
+    this.secretKey = secretKey;
+  }
+
+  @Override
+  public String upload(MultipartFile multipartFile) throws Exception {
+    return null;
+  }
+
+  @Override
+  public void afterPropertiesSet() {
+    log.debug(String.format("endPoint = %s", this.endPoint));
+    log.debug(String.format("regionName = %s", this.regionName));
+    log.debug(String.format("accessKey = %s", this.accessKey));
+    log.debug(String.format("secretKey = %s", this.secretKey));
+  }
+}
