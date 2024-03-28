8d6509b02daef82999c657bffba510cef2c7b7c4
kimsungmo0108 <rlatjdah8585@daum.net>
2024-03-28 15:17:05 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,7 +1,9 @@
 package bitcamp.myapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
 
   public static void main(String[] args) throws Exception {
