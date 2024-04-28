84b977f7c7e88e16a9f9613045fea19e51798cbf
kimsungmo0108 <rlatjdah8585@daum.net>
2024-04-28 18:38:13 +0900

    baekjoon.level07.exam02

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam02/Main.java
@@ -3,7 +3,25 @@ package algorithm.test.baekjoon.level07.exam02;
 public class Main {
   public static void main(String[] args) {
     java.util.Scanner sc = new java.util.Scanner(System.in);
-
+    int[][] num = new int[9][9];
+    int max = -1;
+    int x = -1;
+    int y = -1;
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        num[i][j] = sc.nextInt();
+      }
+    }
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (max < num[i][j]) {
+          max = num[i][j];
+          x = i;
+          y = j;
+        }
+      }
+    }
+    System.out.printf("%d \n%d %d", max, x + 1, y + 1);
 
     sc.close();
   }
