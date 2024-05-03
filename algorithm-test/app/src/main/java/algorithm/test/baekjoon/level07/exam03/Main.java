bd52633802a59d6c21678cd22b80fd6a68318c5e
kimsungmo0108 <rlatjdah8585@daum.net>
2024-05-03 09:20:22 +0900

    baekjoon.level07.exam03

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam03/Main.java
@@ -3,7 +3,29 @@ package algorithm.test.baekjoon.level07.exam03;
 public class Main {
   public static void main(String[] args) {
     java.util.Scanner sc = new java.util.Scanner(System.in);
+    char[] cArr1 = sc.next().toCharArray();
+    char[] cArr2 = sc.next().toCharArray();
+    char[] cArr3 = sc.next().toCharArray();
+    char[] cArr4 = sc.next().toCharArray();
+    char[] cArr5 = sc.next().toCharArray();
 
+    for (int i = 0; i < 15; i++) {
+      if (cArr1.length > i) {
+        System.out.print(cArr1[i]);
+      }
+      if (cArr2.length > i) {
+        System.out.print(cArr2[i]);
+      }
+      if (cArr3.length > i) {
+        System.out.print(cArr3[i]);
+      }
+      if (cArr4.length > i) {
+        System.out.print(cArr4[i]);
+      }
+      if (cArr5.length > i) {
+        System.out.print(cArr5[i]);
+      }
+    }
 
     sc.close();
   }
